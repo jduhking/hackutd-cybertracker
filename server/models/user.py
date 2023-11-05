bb057ea46107944b20830fdb9e072f9b35ba8731
Buchimi <stormzzy900@gmail.com>
2023-11-05 11:55:22 -0600

    hotfix

--- server/models/user.py
+++ server/models/user.py
@@ -1,7 +1,7 @@
 from beanie import Document, PydanticObjectId, Indexed
 from models.badsite import BadSite
 from pydantic import BaseModel, Field
-
+from typing import List
 class User(Document):
     email : Indexed(str)
     name : str 
@@ -13,6 +13,6 @@ class UserProjection(BaseModel):
     id : PydanticObjectId = Field(alias="_id")
     
 class UserOut(User, BaseModel):
-    badvisits  : list[BadSite]
+    badvisits  : List[BadSite]
     def fromUser(user:User , ls : list[BadSite]):
         return( UserOut(email=user.email, name=user.name, total_sites_visited= user.total_sites_visited, phishing_links=user.phishing_links, badvisits=ls))
