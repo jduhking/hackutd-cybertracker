666b42f2830f2ad4399649d201fda3655ddc7294
Buchimi <stormzzy900@gmail.com>
2023-11-04 21:11:24 -0500

    init server

--- server/main.py
+++ server/main.py
@@ -1,9 +1,38 @@
 from typing import Union
-
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from motor.motor_asyncio import AsyncIOMotorClient
+from utils.config import Config
+from beanie import init_beanie
 
-app = FastAPI()
+from models.user import User
 
+@asynccontextmanager
+async def lifespan(app : FastAPI):
+    # before server starts
+    print("Hello")
+    await init(app)
+    yield
+
+    # before server ends
+    print("World")
+    pass
+
+
+
+async def init(app):
+    config = Config()
+    # connect to mongo
+    client = AsyncIOMotorClient(config.mongoConnectionURI())
+    await init_beanie(database=client.db_name, document_models=[User])
+    try:
+        info = client.server_info()
+        print(f"success, connected to {info}")
+    except Exception as e:
+        print(f"Failed with exception {e}")
+    pass
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 async def read_root():
