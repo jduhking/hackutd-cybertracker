53af6629dc9d9801dfede590476e39df657242d5
jduhking <jamesbodebiyi@gmail.com>
2023-11-05 02:53:41 -0600

    stuff

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,38 @@
 
 import './App.css'
+import {useState} from 'react'
 import stateFarm from './assets/statefarm.png'
 import happyJake from './assets/jakehappy.png'
 
+interface BadSite {
+  id: string;
+  risk: string
+}
+
+interface PhishingEmails {
+  id: string;
+  email: string;
+}
+interface UserDetails {
+  id: string;
+  name: string;
+  listOfBadSites?: Array<BadSite>
+  listOfPhishingEmails?: Array<PhishingEmails> 
+  bonus: number;
+  safetyScore: number;
+}
+
 function App() {
 
+  const [userData, setUserData] = useState<UserDetails>({
+    id: '121',
+    name: 'Paul',
+    bonus: 50,
+    safetyScore: 83
+  })
+
   return (
+    userData ? 
     <>
     <div className="outerContainer">
       <div className="navbar">
@@ -23,6 +50,9 @@ function App() {
         <div className="circleBackground"></div>
         <div className="bonusSection">
           <h1 className="savedYourself">You've saved yourself...</h1>
+          <p className="bonus">{userData.bonus} <p className="dollarSign">$</p></p>
+          <p className="bonusDescription">this month through <span className="bonusRed">safe </span>and <span className="bonusRed"> responsible </span> web browsing</p>
+
         </div>
       </div>
       <div className="scoreSection">
@@ -30,6 +60,7 @@ function App() {
       </div>
     </div>
     </>
+    : <><div>There seems to be an error</div></>
   )
 }
 
