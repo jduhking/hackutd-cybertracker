2ce939f1d129a259ffe0561ea9c99cd5c2c9d15f
jduhking <jamesbodebiyi@gmail.com>
2023-11-05 01:45:19 -0600

    Added home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import stateFarm from './assets/statefarm.png'
+import happyJake from './assets/jakehappy.png'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="outerContainer">
+      <div className="navbar">
+        <div>
+          <img src={stateFarm} alt="statefarmlogo" width={236} height={43} />
+        </div>
+        <div>
+          Cyber View
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="topSection">
+        <div className="jakeSection" >
+          <img src={happyJake}/>
+        </div>
+        <div className="circleBackground"></div>
+        <div className="bonusSection">
+          <h1 className="savedYourself">You've saved yourself...</h1>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="scoreSection">
+
+      </div>
+    </div>
     </>
   )
 }
---
+++ client/src/SafetyScore.tsx
@@ -0,0 +1,32 @@
+import './css/SafetyScore.css'
+
+
+interface Props {
+    
+}
+
+
+const SafetyScore: React.FC<Props> = () => {
+
+
+    return (
+    <div className="topFrame">
+        <div className="topBar">
+            <div className="title-logo">
+                
+                <h1 className="title">Cyber View</h1>
+            </div>
+            <div className="navBar">
+                
+            </div>
+        </div>
+        <div className="safetyScoreContainer">
+            <h1 className="title"></h1>
+        </div>
+    </div>
+    )
+
+}
+
+
+export default SafetyScore;
